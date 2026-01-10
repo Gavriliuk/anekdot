11db5ed33b91e2f1f56aefaab5b67489cbd41f5f
Alexander Gavriliuk <gavriliuk@gmail.com>
2026-01-10 18:36:26 +0100

    Rename Greeting to JokeScreen

--- app/src/main/java/fr/anekdot/MainActivity.kt
+++ app/src/main/java/fr/anekdot/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AnekdotTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    JokeScreen(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun JokeScreen(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -40,8 +40,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun JokeScreenPreview() {
     AnekdotTheme {
-        Greeting("Android")
+        JokeScreen("Android")
     }
 }
