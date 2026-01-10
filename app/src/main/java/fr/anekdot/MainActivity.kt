5af79405c05f172077146a706078ffb449df35e1
Alexander Gavriliuk <gavriliuk@gmail.com>
2026-01-10 18:42:11 +0100

    Create a simple layout with buttons

--- app/src/main/java/fr/anekdot/MainActivity.kt
+++ app/src/main/java/fr/anekdot/MainActivity.kt
@@ -1,16 +1,26 @@
 package fr.anekdot
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.anekdot.ui.theme.AnekdotTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JokeScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val text = "Hello $name!"
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Случайный анекдот",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier
+        )
+
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = modifier
+        )
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = {  }) {
+                Text("Следующий")
+            }
+            Button(onClick = {
+                val sendIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, text)
+                    type = "text/plain"
+                }
+                context.startActivity(Intent.createChooser(sendIntent, null))
+            }) {
+                Text("Поделиться")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
