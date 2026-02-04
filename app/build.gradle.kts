c7fedb8ad13fa8b854fdc8ffafb43e44352ef325
Alexander Gavriliuk <gavriliuk@gmail.com>
2026-02-04 20:13:37 +0100

    Set version 5 (2.13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "fr.anekdot"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
